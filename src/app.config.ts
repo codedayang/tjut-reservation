08bf6c4d28cc04054c8724a4dd6ca7ba4b71ad1a
大阳 <codedayang@outlook.com>
2021-04-09 20:54:10 +0800

    代码同步

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,6 +2,7 @@ import { useGlobalIconFont } from './component/iconfont/helper';
 
 export default {
   pages: [
+    'pages/MeetForm/index',
     'pages/index/index'
   ],
   window: {
---
+++ src/pages/MeetForm/index.tsx
@@ -0,0 +1,67 @@
+import Taro from "@tarojs/taro";
+import {Button, Form, Input, Picker, Text, View} from "@tarojs/components";
+import './index.less'
+type Props = {
+
+}
+const MeetForm: Taro.FunctionComponent<Props> = (props) => {
+
+  const handleSubmit = (e) => {
+    console.log(e);
+  }
+
+  return (
+    <View className="container">
+      <Form onSubmit={handleSubmit} className="meet-form">
+        <View className="form-item">
+          <Text className="form-item-left">会议名称</Text>
+          <Input type='text' placeholder='必填' className="form-item-right"/>
+        </View>
+        <View className="divider"/>
+
+        <View className="form-item">
+          <Text className="form-item-left">会议室</Text>
+          <Picker
+            mode="selector"
+            range={["108","109","411"]}
+            onChange={(e) => console.log(e)}
+            className="form-item-right">
+            <View>当前选择</View>
+          </Picker>
+        </View>
+        <View className="divider"/>
+        <View className="form-item">
+          <Text className="form-item-left">日期</Text>
+          <Picker
+            mode="date"
+            value={""}
+            onChange={(e) => console.log(e)}
+            className="form-item-right">
+            <View>当前选择</View>
+          </Picker>
+        </View>
+        <View className="divider"/>
+        <View className="form-item">
+          <Text className="form-item-left">时间</Text>
+          <Picker
+            mode="time"
+            value={""}
+            onChange={(e) => console.log(e)}
+            className="form-item-right">
+            <View>当前选择</View>
+          </Picker>
+        </View>
+        <View className="divider"/>
+        <View className="form-item">
+          <Text className="form-item-left">会议内容</Text>
+          <Input type='text' placeholder='必填' className="form-item-right"/>
+        </View>
+        <View className="divider"/>
+        <Button formType={"submit"} type="primary" style={{margin: "16px"}}>创建</Button>
+
+      </Form>
+    </View>
+  )
+}
+
+export default MeetForm;
